e2a75f09b75a489e2bec0859dfff6f7f6a4e4c3f
Ricke <RickeFelix@web.de>
2019-04-29 11:45:29 +0200

    WIP: Revision of the gui functions regarding multiple table widgets
    Extract methods that handle Tablewidget events into their own Tablewidget class.
    
    methods for multiple tablewidgets:
    - convert to hex/ascii
    - color table cells
    - Add/delete row/column
      (further work may be required)
    
    WIP:
    copy/extract and paste

--- app.py
+++ app.py
@@ -1,9 +1,156 @@
+import binascii
 import csv
 import sys
 
 import clipboard as clipboard
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtWidgets import *
+
+
+class CustomTableWidget(QTableWidget):
+
+    def __init__(self, parent=None):
+        super(CustomTableWidget, self).__init__(parent)
+
+    def onTableClick(self):
+        """
+        Prints cell content on the console
+
+        :return:
+        """
+        print("\n")
+        for currentQTableWidgetItem in self.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
+    def contextMenuEvent(self, event):
+        """
+        Defines a context menu on the table widget.
+        :param event:
+        :return: None
+        """
+        contextMenu = QMenu(self)
+        colorAct = contextMenu.addAction("Color")
+        asciiAct = contextMenu.addAction("Convert to ASCII")
+        hexAct = contextMenu.addAction("Convert to HEX")
+        action = contextMenu.exec_(event.globalPos())
+
+        if action == colorAct:
+            self.openColorDialog()
+        elif action == asciiAct:
+            self.convertToASCII()
+        elif action == hexAct:
+            self.convertToHex()
+
+    def addTableRow(self):
+        """
+        Adds a new row below the selected one.
+
+        :return: None
+        """
+        self.insertRow(self.currentRow() + 1)
+
+    def addTableColumn(self):
+        """
+        Adds a new column to the right of the selected one.
+        :return: None
+        """
+        self.insertColumn(self.currentColumn() + 1)
+
+    def removeTableRow(self):
+        """
+        Removes the selected row
+        :return: None
+        """
+        self.removeRow(self.currentRow())
+
+    def removeTableColumn(self):
+        """
+        Removes the selected column
+        :return: None
+        """
+        self.removeColumn(self.currentColumn())
+
+    def convertToASCII(self):
+        """
+        Converts the table data to ascii
+        :return: None
+        """
+
+        for rowNumber in range(self.rowCount()):
+            for columnNumber in range(self.columnCount()):
+                if self.item(rowNumber, columnNumber) is not None:
+                    self.setItem(rowNumber, columnNumber, QTableWidgetItem(
+                        binascii.unhexlify(self.item(rowNumber, columnNumber).text()).decode('utf8')))
+
+    def convertToHex(self):
+        """
+        Converts the table data to hex
+        :return: None
+        """
+
+        for rowNumber in range(self.rowCount()):
+            for columnNumber in range(self.columnCount()):
+                if self.item(rowNumber, columnNumber) is not None:
+                    binaryData = bytes(self.item(rowNumber, columnNumber).text(), 'utf-8')
+                    hexData = binaryData.hex()
+                    self.setItem(rowNumber, columnNumber, QTableWidgetItem(hexData))
+
+    def openColorDialog(self):
+        """
+        Opens a color dialog to select and set a background color for the selected cells.
+        :return:
+        """
+
+        color = QColorDialog.getColor()
+
+        if color.isValid():
+            for item in self.selectedItems():
+                item.setBackground(color)
+
+    def copyData(self):
+        """
+        Copies the data from the selected cells to the system clipboard
+        :return: None
+        """
+        copiedData = ""
+        for i in range(self.selectedRanges().pop(0).topRow(),
+                       self.selectedRanges().pop(0).bottomRow() + 1):
+            for j in range(self.selectedRanges().pop(0).leftColumn(),
+                           self.selectedRanges().pop(0).rightColumn() + 1):
+                copiedData += self.item(i, j).text() + "\t"
+            copiedData += '\n'
+        clipboard.copy(copiedData)
+
+    def extractData(self):
+        """
+        Extracts the data from the selected cells to the system clipboard and fills the cells with empty strings
+        :return: None
+        """
+        copiedData = ""
+        for i in range(self.selectedRanges().pop(0).topRow(),
+                       self.selectedRanges().pop(0).bottomRow() + 1):
+            for j in range(self.selectedRanges().pop(0).leftColumn(),
+                           self.selectedRanges().pop(0).rightColumn() + 1):
+                copiedData += self.item(i, j).text() + "\t"
+                self.setItem(i, j, QTableWidgetItem(""))
+            copiedData += '\n'
+        clipboard.copy(copiedData)
+
+    def insertData(self):
+        """
+        Inserts the data from the system clipboard to the selected cell.
+        TODO: Fix: When inserting into the last column, the following data is inserted into the initial columns
+        :return: None
+        """
+        data = clipboard.paste().split("\n")
+
+        for i in range(self.selectedRanges().pop(0).topRow(),
+                       self.selectedRanges().pop(0).bottomRow() + len(data) - 1):
+            tokens = data[i - self.selectedRanges().pop(0).topRow()].strip().split("\t")
+            for j in range(self.selectedRanges().pop(0).leftColumn(),
+                           self.selectedRanges().pop(0).rightColumn() + len(tokens)):
+                self.setItem(i, j, QTableWidgetItem(
+                    tokens[j - self.selectedRanges().pop(0).leftColumn()]))
 
 
 class App(QMainWindow):
@@ -17,8 +164,10 @@ class App(QMainWindow):
         self.height = 500
         self.hotkey = {}
 
-        self.tableWidget = QTableWidget()
-        self.tableWidgetPlainText = QTableWidget()
+        # self.customTableWidgetCipher = CustomTableWidget(parent=self)
+        # self.customTableWidgetPlain = CustomTableWidget(parent=self)
+        self.tableWidgetCipher = CustomTableWidget(parent=self)
+        self.tableWidgetPlain = CustomTableWidget(parent=self)
 
         self.okButton = QPushButton("OK")
         self.cancelButton = QPushButton("Cancel")
@@ -39,16 +188,20 @@ class App(QMainWindow):
 
         self.createMenu()
 
-        self.shortcut('Copy', 'Ctrl+C', self.copyData)
-        self.shortcut('Copy', 'Ctrl+X', self.extractData)
-        self.shortcut('Copy', 'Ctrl+V', self.insertData)
+        self.shortcut('Copy_Cipher', 'Ctrl+C', self.tableWidgetCipher.copyData)
+        self.shortcut('Extract_Cipher', 'Ctrl+X', self.tableWidgetCipher.extractData)
+        self.shortcut('Paste_Cipher', 'Ctrl+V', self.tableWidgetCipher.insertData)
 
-        self.tableWidget.horizontalHeader().setStretchLastSection(True)
-        self.tableWidget.doubleClicked.connect(self.onTableClick)
+        self.shortcut('Copy_Plain', 'Ctrl+C', self.tableWidgetPlain.copyData)
+        self.shortcut('Extract_Plain', 'Ctrl+X', self.tableWidgetPlain.extractData)
+        self.shortcut('Paste_Plain', 'Ctrl+V', self.tableWidgetPlain.insertData)
+
+        self.tableWidgetCipher.horizontalHeader().setStretchLastSection(True)
+        self.tableWidgetPlain.horizontalHeader().setStretchLastSection(True)
 
         self.vboxTableWidgets = QVBoxLayout()
-        self.vboxTableWidgets.addWidget(self.tableWidget)
-        self.vboxTableWidgets.addWidget(self.tableWidgetPlainText)
+        self.vboxTableWidgets.addWidget(self.tableWidgetCipher)
+        self.vboxTableWidgets.addWidget(self.tableWidgetPlain)
 
         self.comboBoxGadget.addItem("CBC")
         self.comboBoxGadget.addItem("CFB")
@@ -134,32 +287,38 @@ class App(QMainWindow):
 
         menuItemAddRow = QAction('Add row', self)
         menuItemAddRow.setStatusTip(' Adds a new row below the selected one.')
-        menuItemAddRow.triggered.connect(self.addRow)
+        menuItemAddRow.triggered.connect(self.tableWidgetCipher.addTableRow)
+        menuItemAddRow.triggered.connect(self.tableWidgetPlain.addTableRow)
         editMenu.addAction(menuItemAddRow)
 
         menuItemAddColumn = QAction('Add column', self)
         menuItemAddColumn.setStatusTip('Adds a new column to the right of the selected one.')
-        menuItemAddColumn.triggered.connect(self.addColumn)
+        menuItemAddColumn.triggered.connect(self.tableWidgetCipher.addTableColumn)
+        menuItemAddColumn.triggered.connect(self.tableWidgetPlain.addTableColumn)
         editMenu.addAction(menuItemAddColumn)
 
         menuItemRemoveRow = QAction('Remove row', self)
         menuItemRemoveRow.setStatusTip('Removes the selected row.')
-        menuItemRemoveRow.triggered.connect(self.removeRow)
+        menuItemRemoveRow.triggered.connect(self.tableWidgetCipher.removeTableRow)
+        menuItemRemoveRow.triggered.connect(self.tableWidgetPlain.removeTableRow)
         editMenu.addAction(menuItemRemoveRow)
 
         menuItemRemoveColumn = QAction('Remove column', self)
         menuItemRemoveColumn.setStatusTip('Removes the selected column.')
-        menuItemRemoveColumn.triggered.connect(self.removeColumn)
+        menuItemRemoveColumn.triggered.connect(self.tableWidgetCipher.removeTableColumn)
+        menuItemRemoveColumn.triggered.connect(self.tableWidgetPlain.removeTableColumn)
         editMenu.addAction(menuItemRemoveColumn)
 
         menuItemConvertToASCII = QAction('Convert to ASCII', self)
         menuItemConvertToASCII.setStatusTip('Converts the table data to ASCII')
-        menuItemConvertToASCII.triggered.connect(self.on_menuItemConvertToASCII_clicked)
+        menuItemConvertToASCII.triggered.connect(self.tableWidgetCipher.convertToASCII)
+        menuItemConvertToASCII.triggered.connect(self.tableWidgetPlain.convertToASCII)
         viewMenu.addAction(menuItemConvertToASCII)
 
         menuItemConvertToHEX = QAction('Convert to HEX', self)
         menuItemConvertToHEX.setStatusTip('Converts the table data to HEX')
-        menuItemConvertToHEX.triggered.connect(self.on_menuItemConvertToHEX_clicked)
+        menuItemConvertToHEX.triggered.connect(self.tableWidgetCipher.convertToHex)
+        menuItemConvertToHEX.triggered.connect(self.tableWidgetPlain.convertToHex)
         viewMenu.addAction(menuItemConvertToHEX)
 
         menuItemAbout = QAction('About', self)
@@ -174,106 +333,17 @@ class App(QMainWindow):
         if filename is not None:
             self.writeCsv(filename)
 
-    def on_menuItemConvertToASCII_clicked(self):
-        self.convertToASCII()
-
-    def on_menuItemConvertToHEX_clicked(self):
-        self.convertToHEX()
-
-    def onTableClick(self):
-        """
-        Prints cell content on the console
-
-        :return:
-        """
-        print("\n")
-        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
-
-    def addRow(self):
-        """
-        Adds a new row below the selected one.
-
-        :return: None
-        """
-        self.tableWidget.insertRow(self.tableWidget.currentRow() + 1)
-
-    def addColumn(self):
-        """
-        Adds a new column to the right of the selected one.
-        :return: None
-        """
-        self.tableWidget.insertColumn(self.tableWidget.currentColumn() + 1)
-
-    def removeRow(self):
-        """
-        Removes the selected row
-        :return: None
-        """
-        self.tableWidget.removeRow(self.tableWidget.currentRow())
-
-    def removeColumn(self):
-        """
-        Removes the selected column
-        :return: None
-        """
-        self.tableWidget.removeColumn(self.tableWidget.currentColumn())
-
     def shortcut(self, key_name, key_combo, func):
         """
         Adds a shortcut for a key combination and the function
         :param key_name: Name of the shortcut
         :param key_combo: Key combination
+        :param widget: specific widget
         :param func: function name you want to call with the shortcut
         :return: 
         """
         self.hotkey[key_name] = QtWidgets.QShortcut(QtGui.QKeySequence(key_combo), self)
         self.hotkey[key_name].activated.connect(func)
-
-    def copyData(self):
-        """
-        Copies the data from the selected cells to the system clipboard
-        :return: None
-        """
-        copiedData = ""
-        for i in range(self.tableWidget.selectedRanges().pop(0).topRow(),
-                       self.tableWidget.selectedRanges().pop(0).bottomRow() + 1):
-            for j in range(self.tableWidget.selectedRanges().pop(0).leftColumn(),
-                           self.tableWidget.selectedRanges().pop(0).rightColumn() + 1):
-                copiedData += self.tableWidget.item(i, j).text() + "\t"
-            copiedData += '\n'
-        clipboard.copy(copiedData)
-
-    def extractData(self):
-        """
-        Extracts the data from the selected cells to the system clipboard and fills the cells with empty strings
-        :return: None
-        """
-        copiedData = ""
-        for i in range(self.tableWidget.selectedRanges().pop(0).topRow(),
-                       self.tableWidget.selectedRanges().pop(0).bottomRow() + 1):
-            for j in range(self.tableWidget.selectedRanges().pop(0).leftColumn(),
-                           self.tableWidget.selectedRanges().pop(0).rightColumn() + 1):
-                copiedData += self.tableWidget.item(i, j).text() + "\t"
-                self.tableWidget.setItem(i, j, QTableWidgetItem(""))
-            copiedData += '\n'
-        clipboard.copy(copiedData)
-
-    def insertData(self):
-        """
-        Inserts the data from the system clipboard to the selected cell.
-        TODO: Fix: When inserting into the last column, the following data is inserted into the initial columns
-        :return: None
-        """
-        data = clipboard.paste().split("\n")
-
-        for i in range(self.tableWidget.selectedRanges().pop(0).topRow(),
-                       self.tableWidget.selectedRanges().pop(0).bottomRow() + len(data) - 1):
-            tokens = data[i - self.tableWidget.selectedRanges().pop(0).topRow()].strip().split("\t")
-            for j in range(self.tableWidget.selectedRanges().pop(0).leftColumn(),
-                           self.tableWidget.selectedRanges().pop(0).rightColumn() + len(tokens)):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(
-                    tokens[j - self.tableWidget.selectedRanges().pop(0).leftColumn()]))
 
     def loadCSV(self, filename):
         """
@@ -286,13 +356,13 @@ class App(QMainWindow):
             infile = open(filename, "r")
             lines = infile.readlines()
             infile.close()
-            self.tableWidget.setRowCount(len(lines))
-            self.tableWidget.setColumnCount(len(lines[0].strip().split(",")))
+            self.tableWidgetCipher.setRowCount(len(lines))
+            self.tableWidgetCipher.setColumnCount(len(lines[0].strip().split(",")))
             for i in range(0, len(lines)):
                 tokens = lines[i].strip().split(",")
                 for j in range(0, len(tokens)):
-                    self.tableWidget.setItem(i, j, QTableWidgetItem(tokens[j]))
-            self.tableWidget.resizeColumnsToContents()
+                    self.tableWidgetCipher.setItem(i, j, QTableWidgetItem(tokens[j]))
+            self.tableWidgetCipher.resizeColumnsToContents()
 
     def writeCsv(self, filename):
         """
@@ -303,66 +373,11 @@ class App(QMainWindow):
         data = []
         with open(filename, "w", newline='') as fileOutput:
             writer = csv.writer(fileOutput)
-            for rowNumber in range(self.tableWidget.rowCount()):
-                for columnNumber in range(self.tableWidget.columnCount()):
-                    data.append(self.tableWidget.item(rowNumber, columnNumber).text())
+            for rowNumber in range(self.tableWidgetCipher.rowCount()):
+                for columnNumber in range(self.tableWidgetCipher.columnCount()):
+                    data.append(self.tableWidgetCipher.item(rowNumber, columnNumber).text())
                 writer.writerow(data)
                 data = []
-
-    def contextMenuEvent(self, event):
-        """
-        Defines a context menu on the table widget.
-        :param event:
-        :return: None
-        """
-        contextMenu = QMenu(self)
-        colorAct = contextMenu.addAction("Color")
-        asciiAct = contextMenu.addAction("Convert to ASCII")
-        hexAct = contextMenu.addAction("Convert to HEX")
-        action = contextMenu.exec_(self.mapToParent(event.pos()))
-
-        if action == colorAct:
-            self.openColorDialog()
-        elif action == asciiAct:
-            self.convertToASCII()
-        elif action == hexAct:
-            self.convertToHEX()
-
-    def openColorDialog(self):
-        """
-        Opens a color dialog to select and set a background color for the selected cells.
-        :return:
-        """
-
-        color = QColorDialog.getColor()
-
-        if color.isValid():
-            for item in self.tableWidget.selectedItems():
-                item.setBackground(color)
-
-    def convertToASCII(self):
-        """
-        Converts the table data to ascii
-        :return: None
-        """
-
-        for rowNumber in range(self.tableWidget.rowCount()):
-            for columnNumber in range(self.tableWidget.columnCount()):
-                self.tableWidget.setItem(rowNumber, columnNumber, QTableWidgetItem(
-                    binascii.unhexlify(self.tableWidget.item(rowNumber, columnNumber).text()).decode('utf8')))
-
-    #
-    def convertToHEX(self):
-        """
-        Converts the table data to hex
-        :return: None
-        """
-
-        for rowNumber in range(self.tableWidget.rowCount()):
-            for columnNumber in range(self.tableWidget.columnCount()):
-                binaryData = bytes(self.tableWidget.item(rowNumber, columnNumber).text(), 'utf-8')
-                hexData = binaryData.hex()
-                self.tableWidget.setItem(rowNumber, columnNumber, QTableWidgetItem(hexData))
 
     def openFile(self):
         """
